583388f81dfc07754bb5c3237cb0cec8219fa378
MisaJC <123918601+JCMisa@users.noreply.github.com>
2025-04-24 05:35:58 +0000

    create the sign-in and sign-up forms

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,8 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SigninPage = () => {
-  return <div>SigninPage</div>;
+  return <AuthForm type="sign-in" />;
 };
 
 export default SigninPage;
