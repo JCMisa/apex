aeb09ba2127bf28efffb063bd33b49fb269a06da
MisaJC <123918601+JCMisa@users.noreply.github.com>
2025-05-01 07:43:22 +0000

    change the project headers

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,50 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // async headers() {
+  //   console.log("CODESPACE_NAME:", process.env.CODESPACE_NAME);
+  //   console.log("Using Headers:", [
+  //     {
+  //       key: "X-Forwarded-Host",
+  //       value: `${process.env.CODESPACE_NAME}-3000.app.github.dev`,
+  //     },
+  //     {
+  //       key: "Access-Control-Allow-Origin",
+  //       value: `https://${process.env.CODESPACE_NAME}-3000.app.github.dev`,
+  //     },
+  //   ]);
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: process.env.CODESPACES
+  //         ? [
+  //             {
+  //               key: "X-Forwarded-Host",
+  //               value: `${process.env.CODESPACE_NAME}-3000.app.github.dev`,
+  //             },
+  //             {
+  //               key: "Access-Control-Allow-Origin",
+  //               value: `https://${process.env.CODESPACE_NAME}-3000.app.github.dev`,
+  //             },
+  //           ]
+  //         : [],
+  //     },
+  //   ];
+  // },
   async headers() {
-    console.log("CODESPACE_NAME:", process.env.CODESPACE_NAME);
-    console.log("Using Headers:", [
-      {
-        key: "X-Forwarded-Host",
-        value: `${process.env.CODESPACE_NAME}-3000.app.github.dev`,
-      },
-      {
-        key: "Access-Control-Allow-Origin",
-        value: `https://${process.env.CODESPACE_NAME}-3000.app.github.dev`,
-      },
-    ]);
     return [
       {
         source: "/(.*)",
-        headers: process.env.CODESPACES
-          ? [
-              {
-                key: "X-Forwarded-Host",
-                value: `${process.env.CODESPACE_NAME}-3000.app.github.dev`,
-              },
-              {
-                key: "Access-Control-Allow-Origin",
-                value: `https://${process.env.CODESPACE_NAME}-3000.app.github.dev`,
-              },
-            ]
-          : [],
+        headers: [
+          {
+            key: "X-Forwarded-Host",
+            value: `${process.env.CODESPACE_NAME}-3000.app.github.dev`,
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: `https://${process.env.CODESPACE_NAME}-3000.app.github.dev`,
+          },
+        ],
       },
     ];
   },
